bf6f7b23eef79219535c27473422a96d5e900025
Wellington Grisa <wellington.grisa@gmail.com>
2020-06-22 20:15:44 -0300

    Export interfaces in main index file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { expressYupMiddleware } from './express-yup-middleware'
 export { validatePayload } from './schema-validator'
+export { ErrorMessages, ExpressYupMiddlewareInterface } from './schema-validation-interface'
