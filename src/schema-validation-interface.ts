7a4d782417d8e4a104b7466c3cbec68a769f3afe
Rusty Conover <rusty@conover.me>
2021-03-22 10:29:17 -0400

    Improve the typing of parameters.
    Restruct the data sources to body, query or params.
    
    Change the schema and options to be an exported interface
    so that users can reference the schema without losing the
    typing information when its referenced outside of the
    middleware.

--- src/schema-validation-interface.ts
+++ src/schema-validation-interface.ts
@@ -1,11 +1,28 @@
 import { Shape, ValidateOptions } from 'yup'
 
-interface SchemaValidationInterface {
-  [key: string]: {
-    yupSchema: Shape<any, any>
-    validateOptions?: ValidateOptions
-  }
+export interface SchemaWithOptions {
+  /** The schema to use to validate */
+  yupSchema: Shape<any, any>
+  /** Options to validate the schema with */
+  validateOptions?: ValidateOptions
 }
+
+/**
+ * The source of the data to validate
+ *
+ * * `body` The body of the request
+ * * `query` The query string of the request
+ * * `params` The params of the request
+ * * `headers` The headers of the request
+ */
+export type SchemaValidationAreas<T> = {
+  body?: T
+  query?: T
+  params?: T
+  headers?: T
+}
+
+export type SchemaValidationInterface = SchemaValidationAreas<SchemaWithOptions>
 
 export interface ErrorMessages {
   [key: string]: {
