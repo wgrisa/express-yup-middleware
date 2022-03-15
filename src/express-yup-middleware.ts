4d9123ab5bfc67a289435642fabb7f04c0d9bd20
william <grisa.william@gmail.com>
2022-03-15 11:40:25 -0300

    Remove http status codes dependency

--- src/express-yup-middleware.ts
+++ src/express-yup-middleware.ts
@@ -1,29 +1,32 @@
 import { NextFunction, Request, Response } from 'express'
-import { BAD_REQUEST } from 'http-status-codes'
 
 import { ExpressYupMiddlewareInterface } from './schema-validation-interface'
 import { validatePayload } from './schema-validator'
 
 const defaultPropertiesToValidate = ['params', 'body', 'query']
 
-export const expressYupMiddleware = ({
-  schemaValidator,
-  expectedStatusCode,
-  propertiesToValidate = defaultPropertiesToValidate,
-}: {
-  schemaValidator: ExpressYupMiddlewareInterface
-  expectedStatusCode?: number
-  propertiesToValidate?: string[]
-}) => async (req: Request, res: Response, next: NextFunction) => {
-  const errors = await validatePayload({
+const BAD_REQUEST = 400
+
+export const expressYupMiddleware =
+  ({
     schemaValidator,
-    payload: req,
-    propertiesToValidate,
-  })
+    expectedStatusCode,
+    propertiesToValidate = defaultPropertiesToValidate,
+  }: {
+    schemaValidator: ExpressYupMiddlewareInterface
+    expectedStatusCode?: number
+    propertiesToValidate?: string[]
+  }) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = await validatePayload({
+      schemaValidator,
+      payload: req,
+      propertiesToValidate,
+    })
 
-  if (!errors) {
-    return next()
+    if (!errors) {
+      return next()
+    }
+
+    return res.status(expectedStatusCode || BAD_REQUEST).send({ errors })
   }
-
-  return res.status(expectedStatusCode || BAD_REQUEST).send({ errors })
-}
